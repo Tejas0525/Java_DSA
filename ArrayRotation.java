629dcada8410e5707f850a3a8450663d72eac53c
Tejas0525 <tp07474@gmail.com>
2024-01-03 10:39:08 +0530

    done some changes

--- ArrayRotation.java
+++ ArrayRotation.java
@@ -1,35 +1,3 @@
-/*
- Array Rotation
- Given an array with N elements, the task is to rotate the array to the left by K steps without using any extra space, where K is non-negative
- Input Format
-The first line contains an integer N representing the size of the array
-The second line contains N space-separated integers representing the elements of the array
-The last line contains an integer K representing the number of times the array has to be rotated in the left direction.
-Output Format
-The only line of the output prints N space-separated integers representing the Rotated array elements.
-Note: You need to complete the given function.
-Example 1
-Input
-8
-7 5 2 11 2 43 1 1
-2
-Output 
-2 11 2 43 1 1 7 5
-Explanation Rotate 1 steps to the left: 5 2 11 2 43 1 1 7
-Rotate 2 steps to the left: 2 11 2 43 1 17 5
-}
-Example 2 
-Input
-8
-7 5 2 11 2 43 1 1
-3
-Output 
-11 2 43 1 1 7 5 2 
-Explanation
-Rotate 1 steps to the left: 5 2 11 2 43 1 1 7
-Rotate 2 steps to the left: 2 11 2 43 1 1 7 5
-Rotate 3 steps to the left: 11 2 43 1 1 7 5 2
- */
 
  import java.util.*;
  class ArrayRotation{
--- CalendarDay.java
+++ CalendarDay.java
@@ -4,15 +4,6 @@ import java.util.*;
 
 class Result {
 
-    /*
-     * Complete the 'findDay' function below.
-     *
-     * The function is expected to return a STRING.
-     * The function accepts following parameters:
-     *  1. INTEGER month
-     *  2. INTEGER day
-     *  3. INTEGER year
-     */
 
     public static String findDay(int month, int day, int year) {
             Calendar calendar = Calendar.getInstance();
