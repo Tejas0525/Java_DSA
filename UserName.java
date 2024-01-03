a33f14b19e9ff49f2f4262c8153df02491612fbe
Tejas0525 <tp07474@gmail.com>
2024-01-03 10:33:27 +0530

    "UserName"

--- UserName.java
+++ UserName.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class UsernameValidator {
     
-    public static final String regularExpression = "^[a-zA-Z]\\w{7,29}$";
+    public static final String regularExpression = "[a-zA-Z]\\w{7,29}$";
 }
 
 public class Solution {
